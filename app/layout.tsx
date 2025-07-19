f368c65440c4d6b07bda58bb5291291746cb37d2
asaduzzaman69 <asaduzzamanhimel12@gmail.com>
2025-07-19 22:27:51 +0600

    chore: add base Clerk setup before auth implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import {
+  ClerkProvider,
+
+} from '@clerk/nextjs'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,6 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
+
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -30,5 +36,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
